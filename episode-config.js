384ee8a449c0c996c54515eeca9d31b7ebf76216
HalluxX <austinNmarks@aol.com>
2025-08-28 17:50:29 -0700

    chanel video

---
+++ episode-config.js
@@ -0,0 +1,15 @@
+// Episode Configuration File
+// Update this file to change video sources for different episodes
+
+const episodeConfig = {
+    // Current episode configuration
+    currentEpisode: {
+        type: 'vimeo', // 'local', 'youtube', or 'vimeo'
+        title: 'Episode Three',
+        thumbnail: '',
+        localSrc: '',
+        youtubeId: '',
+        vimeoId: '1114095611'
+    }
+};
+
