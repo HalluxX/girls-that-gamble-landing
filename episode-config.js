55afd700b71d14047fd53f9434b53ff3024d85a5
HalluxX <austinNmarks@aol.com>
2025-08-31 13:28:25 -0700

    removed git ignore for mp4

--- episode-config.js
+++ episode-config.js
@@ -2,7 +2,7 @@
 // Update this file to change video sources for different episodes
 
 const episodeConfig = {
-    // Current episode configuration
+    // Current episode configuration added to new episode
     currentEpisode: {
         type: 'local', // 'local', 'youtube', or 'vimeo'
         title: 'College 8/31 notre dame vs miami',
