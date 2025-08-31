e7013eccab23b8fea8b9241501c884ed1195ecc5
HalluxX <austinNmarks@aol.com>
2025-08-31 13:08:12 -0700

    updated local video ref

--- episode-config.js
+++ episode-config.js
@@ -7,7 +7,7 @@ const episodeConfig = {
         type: 'local', // 'local', 'youtube', or 'vimeo'
         title: 'College 8/31 notre dame vs miami',
         thumbnail: '',
-        localSrc: '/ncaaf_8_31.mp4',
+        localSrc: 'ncaaf_8_31.mp4',
         youtubeId: '',
         vimeoId: '',
         vimeoUrl: '' 
